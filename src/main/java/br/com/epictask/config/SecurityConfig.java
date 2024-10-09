08ac065be4edbacef7f130db49a94a89c5425152
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-09 08:39:36 -0300

    criação das classes de autenticação e segurança

---
+++ src/main/java/br/com/epictask/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package br.com.epictask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(login -> login.loginPage("/login").permitAll())
+                //.formLogin(Customizer.withDefaults())
+        ;
+        return http.build();
+    }
+
+}
