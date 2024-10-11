30626e1c36c3ebd143cb0090d3920de17b961279
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-11 09:12:26 -0300

    update do arquivo main para rodar com a nova dependencia

--- src/main/java/br/com/epictask/EpictaskApplication.java
+++ src/main/java/br/com/epictask/EpictaskApplication.java
@@ -2,12 +2,43 @@ package br.com.epictask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EpictaskApplication {
 
 	public static void main(String[] args) {
+		// Carregar o arquivo .env usando Dotenv
+		Dotenv dotenv = Dotenv.load();
+
+		// Configurar as variáveis do .env como variáveis de ambiente
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		System.setProperty("RABBITMQ_HOST", dotenv.get("RABBITMQ_HOST"));
+		System.setProperty("RABBITMQ_PORT", dotenv.get("RABBITMQ_PORT"));
+		System.setProperty("RABBITMQ_USERNAME", dotenv.get("RABBITMQ_USERNAME"));
+		System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
+
+		System.setProperty("MAIL_HOST", dotenv.get("MAIL_HOST"));
+		System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+
+		System.setProperty("SECURITY_USER_NAME", dotenv.get("SECURITY_USER_NAME"));
+		System.setProperty("SECURITY_USER_PASSWORD", dotenv.get("SECURITY_USER_PASSWORD"));
+
+		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+		System.setProperty("FACEBOOK_CLIENT_ID", dotenv.get("FACEBOOK_CLIENT_ID"));
+		System.setProperty("FACEBOOK_CLIENT_SECRET", dotenv.get("FACEBOOK_CLIENT_SECRET"));
+
+		// Iniciar a aplicação Spring Boot
 		SpringApplication.run(EpictaskApplication.class, args);
 	}
-
 }
